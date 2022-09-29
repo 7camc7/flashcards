ef43314a2fcc32de0238f8de2793a4737cd08c7e
7camc7 <108956844+7camc7@users.noreply.github.com>
2022-09-28 20:32:01 -0600

    first

---
+++ main.py
@@ -0,0 +1,67 @@
+from tkinter import *
+import pandas
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_bg, image=front_img)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def known_card():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
+def flip_card():
+    canvas.itemconfig(card_bg, image=back_img)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+
+
+window = Tk()
+window.title("Spanish Flashcards")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(width=800, height=526)
+front_img = PhotoImage(file="card_front.png")
+back_img = PhotoImage(file="card_back.png")
+card_bg = canvas.create_image(400, 263, image=front_img)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 40, "bold"))
+canvas.grid(column=0, row=0, columnspan=2)
+
+cross_img = PhotoImage(file="wrong.png")
+cross_button = Button(image=cross_img, highlightthickness=0, command=next_card)
+cross_button.grid(column=0, row=1)
+
+tick_img = PhotoImage(file="right.png")
+tick_button = Button(image=tick_img, highlightthickness=0, command=known_card)
+tick_button.grid(column=1, row=1)
+
+next_card()
+
+
+
+window.mainloop()
